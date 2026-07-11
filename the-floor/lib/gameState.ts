6dd9d362912963be73c7cc2019450092cf3300d1
Mateusz Okon <mateusz.okon04@gmail.com>
2026-07-11 17:00:05 +0200

    feat: create  function to return remaining game time

--- the-floor/lib/gameState.ts
+++ the-floor/lib/gameState.ts
@@ -1,9 +1,33 @@
-
 export type GameState = {
-    activePlayer: string | 'A' | 'B';
+    activePlayer: 'A' | 'B';
     timeA: number;
     timeB: number;
-    category: string;
+    category: null | string;
+    photoIndex: null | number;
     isRunning: boolean;
-
+    deadline: null | number;
 }
+
+
+function remaining(state: GameState): number {
+    if(state.isRunning === true && state.deadline){
+        return Math.max(0, state.deadline - Date.now());
+    }
+    else if(state.activePlayer === 'A'){
+        return state.timeA;
+    } 
+    else{
+        return state.timeB
+    }
+}
+
+// function switch_player(state: GameState): void{
+//     if(state.isRunning && state.activePlayer === 'A'){
+//         state.timeA = Math.max(0, state.deadline - Date.now());
+//         state.activePlayer = 'B';
+//     }
+//     else{
+//         state.timeB = Math.max(0, state.deadline - Date.now());
+//         state.activePlayer = 'A';
+//     }
+// }
