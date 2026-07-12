694a1714d4964e52f75e77362698cb0d1f61da47
Mateusz Okon <mateusz.okon04@gmail.com>
2026-07-12 22:05:57 +0200

    feat: add switch player and pause functions

--- the-floor/lib/gameState.ts
+++ the-floor/lib/gameState.ts
@@ -21,13 +21,36 @@ function remaining(state: GameState): number {
     }
 }
 
-// function switch_player(state: GameState): void{
-//     if(state.isRunning && state.activePlayer === 'A'){
-//         state.timeA = Math.max(0, state.deadline - Date.now());
-//         state.activePlayer = 'B';
-//     }
-//     else{
-//         state.timeB = Math.max(0, state.deadline - Date.now());
-//         state.activePlayer = 'A';
-//     }
-// }
+function switch_player(state: GameState): void {
+    if(state.isRunning && state.activePlayer === 'A' && state.deadline){
+        state.timeA = Math.max(0, state.deadline - Date.now());
+        state.activePlayer = 'B';
+        state.deadline = Date.now() + state.timeB;
+    }
+    else if(state.isRunning && state.activePlayer === 'B' && state.deadline){
+        state.timeB = Math.max(0, state.deadline - Date.now());
+        state.activePlayer = 'A';
+        state.deadline = Date.now() + state.timeA;
+    }
+}
+
+function freeze_game(state: GameState): void {
+    if(state.isRunning && state.activePlayer === 'A' && state.deadline){
+        state.isRunning = false;
+        state.timeA = Math.max(0, state.deadline - Date.now());
+    } 
+    else if(state.isRunning && state.activePlayer === 'B' && state.deadline) {
+        state.isRunning = false;
+        state.timeB = Math.max(0, state.deadline - Date.now());
+    }
+    else{
+        if(state.activePlayer === 'B'){
+            state.isRunning = true; 
+            state.deadline = Date.now() + state.timeB;
+        }
+        else if(state.activePlayer === 'A'){
+            state.isRunning = true; 
+            state.deadline = Date.now() + state.timeA;
+        }
+    }
+}
